50df26057269340061d3be0466d80d2cde474035
Diego VB <blldgg@forem.be>
2025-06-20 17:04:35 +0200

    Add new dependencies

--- blocks/contact/contact.js
+++ blocks/contact/contact.js
@@ -1,5 +1,5 @@
-import htm from '/node_modules/htm/dist/html.js';
-import { h, render } from '/node_modules/preact/dist/preact.js';
+import htm from '../../node_modules/htm/dist/htm.module.js';
+import { h, render } from '../../node_modules/preact/dist/preact.module.js';
 
 const person = {
     name: '',
@@ -9,16 +9,13 @@ const person = {
 const html = htm.bind(h);
 
 export default async function decorate(block) {
-    console.log(block);
     person.name = block.getElementsByTagName('p')[0].textContent.trim();
     person.email = block.getElementsByTagName('p')[1].textContent.trim();
-    console.log(`Name: ${person.name}, Email: ${person.email}`);
-    const emailLink = document.createElement('a');
-    emailLink.href = `mailto:${person.email}`;
-    emailLink.innerText = person.email;
-    emailLink.className = 'contact-email';
-    block.replaceChildren(html`<${PersonCard} name=${person.name} email=${person.email}/>`);
-}
+    block.innerHTML = ''; 
+render(
+      html`<${PersonCard} name=${person.name} email=${person.email} />`,
+      block
+    );}
 
 const PersonCard = ({ name, email }) => html`
   <div class="card">
