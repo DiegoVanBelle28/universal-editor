5492b57af698c78a711fad19893a2958360d614a
Diego VB <blldgg@forem.be>
2025-06-20 15:52:35 +0200

    Add HTML to contact component

--- blocks/contact/contact.js
+++ blocks/contact/contact.js
@@ -1,4 +1,10 @@
 export default async function decorate(block) {
     console.log('Contact block loaded');
     console.log('Block element:', block);
+    const [blockWrapper] = block.children;
+    const emailLink = document.createElement('a');
+    emailLink.href = `mailto:${blockWrapper.textContent.trim()}`;
+    emailLink.textContent = blockWrapper.textContent.trim();
+    emailLink.className = 'contact-email';
+    blockWrapper.replaceChildren(emailLink);
 }
